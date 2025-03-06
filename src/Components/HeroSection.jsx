93636703e49a986523526c5ee8f1b725f0451f9b
Mateennazir <mateennazir0@gmail.com>
2025-03-06 12:19:42 +0530

    Completed the navBar and some work on Hero section

---
+++ src/Components/HeroSection.jsx
@@ -0,0 +1,17 @@
+import video1 from "../assets/video1.mp4";
+import video2 from "../assets/video2.mp4";
+
+const HeroSection = () => {
+  return (
+    <div className="flex flex-col item-center mt-6 lg:mt-20">
+      <h1 className="text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide">
+        NeoVerse build tools
+        <span className="bg-gradient-to-r from-orange-500 to-red-800 text-transparent bg-clip-text">
+          {" "}
+          for developers
+        </span>
+      </h1>
+    </div>
+  );
+};
+export default HeroSection;
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,15 @@
+import { Menu, X } from "lucide-react";
+import { useState } from "react";
 import React from "react";
 import logo from "../assets/logo.png";
 import { navItems } from "../constants";
 
 const Navbar = () => {
+  const [mobileDrawerOpen, setmobileDrawerOpen] = useState(false);
+
+  const toggleNavbar = () => {
+    setmobileDrawerOpen(!mobileDrawerOpen);
+  };
   return (
     <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/80">
       <div className="container px-4 mx-auto relative text-sm">
@@ -30,7 +37,34 @@ const Navbar = () => {
               Create an account
             </a>
           </div>
+          <div className="lg:hidden md:flex flex-col justify-end">
+            <button onClick={toggleNavbar}>
+              {mobileDrawerOpen ? <X /> : <Menu />}
+            </button>
+          </div>
         </div>
+        {mobileDrawerOpen && (
+          <div className="fixed right-0 z-20 bg-neutral-900 w-full p-12 flex flex-col justify-center item-center lg:hidden">
+            <ul>
+              {navItems.map((item, index) => (
+                <li key={index} className="py-4">
+                  <a href={item.href}>{item.label}</a>
+                </li>
+              ))}
+            </ul>
+            <div className="flex space-x-6">
+              <a href="#" className="py-2 px-3 border rounded-md">
+                Sign in
+              </a>
+              <a
+                href="#"
+                className="py-2 px-3 rounded-md bg-gradient-to-r from-orange-500 to-orange-800"
+              >
+                Create an account
+              </a>
+            </div>
+          </div>
+        )}
       </div>
     </nav>
   );
